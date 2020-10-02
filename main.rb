c846ca21f845b3e0c98162ad4c81da3154a6c8cc
Nkuria <nkuria01@gmail.com>
2020-10-02 13:06:57 +0300

    Worked on 2nd bubblesort method

--- main.rb
+++ main.rb
@@ -17,3 +17,23 @@ def bubble_sort(arr)
   end
   
   print bubble_sort([3, 6, 1, 2, 4, 2, 7, 5, 0, 9, 6])
+
+  def bubble_sort_by(arr)
+    range = arr.length - 1
+    loop do
+      swapped = false
+      range.times do |i|
+        if yield(arr[i], arr[i + 1]).positive?
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+          swapped = true
+        end
+      end
+      break if swapped == false
+    end
+    print arr
+  end
+  
+  bubble_sort_by(%w[hi hello hey]) do |left, right|
+    left.length - right.length
+  end
+  
